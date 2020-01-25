d6e7308a6bfc435f72ccbd364ca17f673ecd7d49
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-01-25 17:04:30 +0100

    changes to https profile

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require('path');
-const http = require('http');
+const https = require('https');
 
 const express = require('express');
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(app);
 
 const socketio = require('socket.io');
 const io = socketio(server);
